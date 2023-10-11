af771fa556db865a49867cd8f9acf7f6edbd8b14
lesimor <lesimor@naver.com>
2023-10-11 23:08:26 +0900

    Add analyze_trend

--- rushguard/scaler/buffer.py
+++ rushguard/scaler/buffer.py
@@ -1,6 +1,7 @@
 import datetime
 
 import numpy as np
+import pandas as pd
 
 
 def calculate_buffer(
@@ -14,3 +15,32 @@ def calculate_buffer(
         return default
     elapsed = now - t_start
     return int(y0 * np.exp(-k * elapsed.total_seconds()))
+
+
+def analyze_trend(series, short_period=5, long_period=20) -> float:
+    """
+    Analyze trend by comparing short-term and long-term averages.
+
+    Parameters:
+    - series: A pandas Series of time series values.
+    - short_period: The period for the short-term average.
+    - long_period: The period for the long-term average.
+
+    Returns:
+    - difference between short-term and long-term averages. \
+        Positive values indicate an upward trend, negative \
+        values indicate a downward trend.
+    """
+
+    short_avg = series[-short_period:].mean()
+    long_avg = series[-long_period:].mean()
+
+    return short_avg - long_avg
+
+
+if __name__ == "__main__":
+    # Example usage
+    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5]
+    series = pd.Series(data)
+    trend = analyze_trend(series, short_period=2)
+    print(trend)
